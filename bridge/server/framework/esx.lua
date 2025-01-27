7264afe6082002e38667185faf5dfce0b31c0538
IlMelons <melonss2002@gmail.com>
2025-01-27 12:25:39 +0100

    Refactored Code
    Updated server.GetDeathState function, thanks to ESX Team (@feelfreetofee and @Kenshiin13)

--- bridge/server/framework/esx.lua
+++ bridge/server/framework/esx.lua
@@ -2,13 +2,11 @@
 
 if GetResourceState("es_extended") ~= "started" then return end
 
-local ESX = exports["es_extended"]:getSharedObject()
-
 server = {}
 
 function server.GetDeathState(src)
-    local xPlayer = ESX.GetPlayerFromId(src)
-    local isDead = xPlayer.PlayerData.dead
+    local playerState = Player(src).state
+    local isDead = playerState.dead
 
     return isDead
 end
